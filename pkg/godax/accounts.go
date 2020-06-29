ce3e8c22167b2ef49a86e29b211f671eaaf72346
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-06-28 19:25:59 -0600

    pkg/godax/{accounts,godax}: add GetAccount (by ID) method

--- pkg/godax/accounts.go
+++ pkg/godax/accounts.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// Account represents a trading account for a coinbase pro profile.
+// ListAccount represents a trading account for a coinbase pro profile.
 /*
 	{
 		"id": "71452118-efc7-4cc4-8780-a5e22d4baa53",
@@ -18,7 +18,7 @@ import (
 		"profile_id": "75da88c5-05bf-4f54-bc85-5c775bd68254"
 	}
 */
-type Account struct {
+type ListAccount struct {
 	// ID - the account ID associated with the coinbase pro profile
 	ID string `json:"id"`
 	// Currency - the currency of the account
@@ -31,31 +31,81 @@ type Account struct {
 	Hold string `json:"hold"`
 }
 
+// Account describes information for a single account
+/*
+	{
+		"id": "a1b2c3d4",
+		"balance": "1.100",
+		"holds": "0.100",
+		"available": "1.00",
+		"currency": "USD"
+	}
+*/
+type Account struct {
+	// ID - the account ID associated with the coinbase pro profile
+	ID string `json:"id"`
+	// Balance - the total funds in the account
+	Balance string `json:"balance"`
+	// Holds - funds on hold (not available for use)
+	Holds string `json:"holds"`
+	// Available - funds available to withdraw or trade
+	Available string `json:"available"`
+	// Currency - the currency of the account
+	Currency string `json:"currency"`
+}
+
 // listAccounts gets a list of trading accounts from the profile associated with the API key.
-func (c *Client) listAccounts() ([]Account, error) {
+func (c *Client) listAccounts() ([]ListAccount, error) {
 	path := "/accounts"
 
 	req, err := http.NewRequest(http.MethodGet, c.baseRestURL+path, nil)
 	if err != nil {
-		return nil, err
+		return []ListAccount{}, err
 	}
 
-	ts := strconv.FormatInt(time.Now().Unix(), 10)
-	sig, err := c.generateSignature(ts, path, http.MethodGet, "")
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	sig, err := c.generateSignature(timestamp, path, http.MethodGet, "")
 	if err != nil {
-		return nil, err
+		return []ListAccount{}, err
 	}
 
-	c.setHeaders(req, ts, sig)
-
+	c.setHeaders(req, timestamp, sig)
 	res, err := c.httpClient.Do(req)
 	if err != nil {
-		return nil, err
+		return []ListAccount{}, err
 	}
 	defer res.Body.Close()
 
-	var accounts []Account
-	json.NewDecoder(res.Body).Decode(&accounts)
+	var acts []ListAccount
+	json.NewDecoder(res.Body).Decode(&acts)
 
-	return accounts, nil
+	return acts, nil
+}
+
+// getAccount retrieves information for a single account.
+func (c *Client) getAccount(accountID string) (Account, error) {
+	path := "/accounts/" + accountID
+
+	req, err := http.NewRequest(http.MethodGet, c.baseRestURL+path, nil)
+	if err != nil {
+		return Account{}, err
+	}
+
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	sig, err := c.generateSignature(timestamp, path, http.MethodGet, "")
+	if err != nil {
+		return Account{}, err
+	}
+
+	c.setHeaders(req, timestamp, sig)
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return Account{}, err
+	}
+	defer res.Body.Close()
+
+	var act Account
+	json.NewDecoder(res.Body).Decode(&act)
+
+	return act, nil
 }
