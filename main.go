63a441b0bb1e87a32b0ce04e619eee78808e4bd8
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-06-28 18:36:04 -0600

    main: going to use main as a scratch pad for playing with the client

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/bradford-hamilton/godax/pkg/godax"
+)
+
+func main() {
+	client, err := godax.NewSandboxClient()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	accounts, err := client.ListAccounts()
+	fmt.Printf("%+v", accounts)
+}
