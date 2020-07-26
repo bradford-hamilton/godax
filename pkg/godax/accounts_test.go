b2992718379a8f04a3e28bb10b4e71531991ce8c
SunkPlane29 <andrezito300@gmail.com>
2020-07-26 13:48:52 -0300

    fixed test in wrong place

--- pkg/godax/accounts_test.go
+++ pkg/godax/accounts_test.go
@@ -560,3 +560,60 @@ func TestClient_ListCoinbaseAccounts(t *testing.T) {
 		})
 	}
 }
+
+func TestClient_GetTrailingVolume(t *testing.T) {
+	tests := [...]struct {
+		name    string
+		fields  fields
+		want    []UserAccount
+		wantRaw string
+		wantErr bool
+	}{
+		{
+			name:   "when a successful call is made to get a users trailing volumes",
+			fields: defaultFields(),
+			want: []UserAccount{{
+				ProductID:      "BTC-USD",
+				ExchangeVolume: "11800.00000000",
+				Volume:         "100.00000000",
+				RecordedAt:     "1973-11-29T00:05:01.123456Z",
+			}},
+			wantRaw: `[{
+				"product_id": "BTC-USD",
+				"exchange_volume": "11800.00000000",
+				"volume": "100.00000000",
+				"recorded_at": "1973-11-29T00:05:01.123456Z"
+			}]`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockClient := MockResponse(tt.wantRaw)
+
+			c := &Client{
+				baseRestURL: tt.fields.baseRestURL,
+				baseWsURL:   tt.fields.baseWsURL,
+				key:         tt.fields.key,
+				secret:      tt.fields.secret,
+				passphrase:  tt.fields.passphrase,
+				httpClient:  mockClient,
+			}
+
+			got, err := c.GetTrailingVolume()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Client.GetTrailingVolume() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if len(c.httpClient.(*MockClient).Requests) != 1 {
+				t.Errorf("should have made one request, but made: %d", len(c.httpClient.(*MockClient).Requests))
+			}
+
+			validateHeaders(t, c)
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Client.GetTrailingVolume() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
