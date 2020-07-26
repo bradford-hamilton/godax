a3e07a55ea92227613d9b864408fbbbd45a3ebd9
SunkPlane29 <andrezito300@gmail.com>
2020-07-26 13:40:59 -0300

    move test to accounts_test.go

--- pkg/godax/accounts_test.go
+++ pkg/godax/accounts_test.go
@@ -433,3 +433,130 @@ func TestClient_GetAccountHolds(t *testing.T) {
 		})
 	}
 }
+
+// TODO: accounts_test.go ???
+func TestClient_ListCoinbaseAccounts(t *testing.T) {
+	tests := [...]struct {
+		name    string
+		fields  fields
+		want    []CoinbaseAccount
+		wantRaw string
+		wantErr bool
+	}{
+		{
+			name:   "when a successful call is made to list coinbase (non-pro) accounts",
+			fields: defaultFields(),
+			want: []CoinbaseAccount{{
+				ID:       "2ae3354e-f1c3-5771-8a37-6228e9d239db",
+				Name:     "USD Wallet",
+				Balance:  "0.00",
+				Currency: "USD",
+				Type:     "fiat",
+				Primary:  false,
+				Active:   true,
+				WireDepositInformation: WireDepositInfo{
+					AccountNumber: "0199003122",
+					RoutingNumber: "026013356",
+					BankName:      "Metropolitan Commercial Bank",
+					BankAddress:   "99 Park Ave 4th Fl New York, NY 10016",
+					BankCountry: BankCountry{
+						Code: "US",
+						Name: "United States",
+					},
+					AccountName:    "Coinbase, Inc",
+					AccountAddress: "548 Market Street, #23008, San Francisco, CA 94104",
+					Reference:      "BAOCAEUX",
+				},
+			}, {
+				ID:       "2a11354e-f133-5771-8a37-622be9b239db",
+				Name:     "EUR Wallet",
+				Balance:  "0.00",
+				Currency: "EUR",
+				Type:     "fiat",
+				Primary:  false,
+				Active:   true,
+				SepaDepositInformation: SepaDepositInfo{
+					IBAN:            "EE957700771001355096",
+					Swift:           "LHVBEE22",
+					BankName:        "AS LHV Pank",
+					BankAddress:     "Tartu mnt 2, 10145 Tallinn, Estonia",
+					BankCountryName: "Estonia",
+					AccountName:     "Coinbase UK, Ltd.",
+					AccountAddress:  "9th Floor, 107 Cheapside, London, EC2V 6DN, United Kingdom",
+					Reference:       "CBAEUXOVFXOXYX",
+				},
+			}},
+			wantErr: false,
+			wantRaw: `[{
+				"id": "2ae3354e-f1c3-5771-8a37-6228e9d239db",
+				"name": "USD Wallet",
+				"balance": "0.00",
+				"currency": "USD",
+				"type": "fiat",
+				"primary": false,
+				"active": true,
+				"wire_deposit_information": {
+					"account_number": "0199003122",
+					"routing_number": "026013356",
+					"bank_name": "Metropolitan Commercial Bank",
+					"bank_address": "99 Park Ave 4th Fl New York, NY 10016",
+					"bank_country": {
+						"code": "US",
+						"name": "United States"
+					},
+					"account_name": "Coinbase, Inc",
+					"account_address": "548 Market Street, #23008, San Francisco, CA 94104",
+					"reference": "BAOCAEUX"
+				}
+			}, {
+				"id": "2a11354e-f133-5771-8a37-622be9b239db",
+				"name": "EUR Wallet",
+				"balance": "0.00",
+				"currency": "EUR",
+				"type": "fiat",
+				"primary": false,
+				"active": true,
+				"sepa_deposit_information": {
+					"iban": "EE957700771001355096",
+					"swift": "LHVBEE22",
+					"bank_name": "AS LHV Pank",
+					"bank_address": "Tartu mnt 2, 10145 Tallinn, Estonia",
+					"bank_country_name": "Estonia",
+					"account_name": "Coinbase UK, Ltd.",
+					"account_address": "9th Floor, 107 Cheapside, London, EC2V 6DN, United Kingdom",
+					"reference": "CBAEUXOVFXOXYX"
+				}
+			}]`,
+		},
+	}
+	for _, tt := range tests {
+		mockClient := MockResponse(tt.wantRaw)
+
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Client{
+				baseRestURL: tt.fields.baseRestURL,
+				baseWsURL:   tt.fields.baseWsURL,
+				key:         tt.fields.key,
+				secret:      tt.fields.secret,
+				passphrase:  tt.fields.passphrase,
+				httpClient:  mockClient,
+			}
+
+			got, err := c.ListCoinbaseAccounts()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Client.ListCoinbaseAccounts() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if len(c.httpClient.(*MockClient).Requests) != 1 {
+				t.Errorf("should have made one request, but made: %d", len(c.httpClient.(*MockClient).Requests))
+			}
+
+			validateHeaders(t, c)
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Client.ListCoinbaseAccounts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
